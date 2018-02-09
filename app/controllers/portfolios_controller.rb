40a1a1ecaf2cc3f015c192eb82495d3c197d3e93
Zerosend <ahern.jason@gmail.com>
2018-02-08 18:56:17 -0800

    Added linking on the index portfolio page

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -8,6 +8,10 @@ class PortfoliosController < ApplicationController
     @portfolio_items = Portfolio.new
   end
 
+  def show
+    @portfolio_items = Portfolio.find(params[:id])
+  end
+
   def create
     @portfolio_items = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
 
@@ -26,7 +30,7 @@ class PortfoliosController < ApplicationController
 
   def update
     @portfolio_items = Portfolio.find(params[:id])
-    
+
     respond_to do |format|
       if @portfolio_items.update(params.require(:portfolio).permit(:title, :subtitle, :body))
         format.html {redirect_to portfolios_path, notice: 'Portfolio Item Update.'}
