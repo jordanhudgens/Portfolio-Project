3a1fdec4cc4b7e9b02e01a59b85be95f3944eed3
Zerosend <ahern.jason@gmail.com>
2018-02-16 14:55:30 -0800

    added copyright into layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,5 +5,18 @@ class ApplicationController < ActionController::Base
   include CurrentUserConcern
   include DefaultPageContent
 
+  before_action :set_copyright
 
+  def set_copyright
+    @copyright = BluTayViewTool::Renderer.copyright 'BluTay Marketing', 'All rights reserved.'
+  end
+end
+
+
+module BluTayViewTool
+  class Renderer
+    def self.copyright name, msg
+      "&copy; #{Time.now.year} | <b>#{name}</b> #{msg}".html_safe
+    end
+  end
 end
