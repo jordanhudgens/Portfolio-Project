13c831f0fd6e30497a702a188ced503e079091ac
Zerosend <ahern.jason@gmail.com>
2018-02-28 20:14:11 -0800

    implemented refactor for navigation and active status

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -19,4 +19,46 @@ module ApplicationHelper
   def copyright name, msg
       "&copy; #{Time.now.year} | <b>#{name}</b> #{msg}".html_safe
   end
+
+  def nav_items
+    [
+      {
+        url: root_path,
+        title: 'Home'
+      },
+      {
+        url: about_us_path,
+        title: 'About Us'
+      },
+      {
+        url: contact_path,
+        title: 'Contact'
+      },
+      {
+        url: blogs_path,
+        title: 'Blog'
+      },
+      {
+        url: portfolios_path,
+        title: 'Portfolio'
+      }
+    ]
+  end
+
+  def nav_helper style, tag_type  
+      nav_links = ''
+
+      nav_items.each do |item|
+        nav_links << "<#{tag_type}><a href='#{item[:url]}' class='#{style} #{active? item[:url]}'>#{item[:title]}</a></#{tag_type}>"
+
+
+      end
+    
+
+    nav_links.html_safe
+  end
+
+  def active? path
+    "active" if current_page? path
+  end
 end
